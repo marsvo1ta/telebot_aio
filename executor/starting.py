89d7c9866b8738bb3cea7aad38aa7af78b7dc429
marsvo1ta <euromobilco@gmail.com>
2023-09-01 16:43:44 +0300

    modify execution

--- executor/starting.py
+++ executor/starting.py
@@ -1,18 +1,11 @@
-from aiogram import executor
-import asyncio
+from aiogram.utils import executor
+from app import CHAT_ID
+from handlers import *
 
-from app import dp, app, bot, CHAT_ID
 
-
-async def on_startup(dp):
-    await bot.send_message(chat_id=CHAT_ID, text="Бот запущений")
-
-    while True:
-        try:
-            await dp.skip_updates()
-            await asyncio.sleep(5)
-        except Exception as e:
-            print(f"Помилка лонгполінгу: {e}")
+async def notify_message(dp): # THIS FUNCTION
+    print('Bot is starting')
+    await dp.bot.send_message(CHAT_ID, text="Bot is starting")
 
 if __name__ == "__main__":
-    executor.start_polling(dp, on_startup=on_startup)
+    executor.start_polling(dp, skip_updates=True, on_startup=notify_message)

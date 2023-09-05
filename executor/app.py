f2b632ef5f0b6489afc6bb21187aa66f56db2847
marsvo1ta <euromobilco@gmail.com>
2023-09-05 10:09:05 +0300

    change folder

--- executor/app.py
+++ executor/app.py
@@ -1,9 +1,9 @@
 import os
 from aiogram import Bot, Dispatcher
-from fastapi import FastAPI
+# from fastapi import FastAPI
 
 
-app = FastAPI()
+# app = FastAPI()
 TOKEN = os.environ.get('TELEGRAM_TOKEN')
 CHAT_ID = os.environ.get('HEALTH_CHECK_CHAT_ID')
 bot = Bot(token=TOKEN)

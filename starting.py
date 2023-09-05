f3fd8ec0afab598881e90c12227aaad0756decdd
marsvo1ta <84044344+marsvo1ta@users.noreply.github.com>
2023-09-05 22:58:05 +0300

    Update starting.py

--- starting.py
+++ starting.py
@@ -4,10 +4,18 @@
 #from executor.app import app
 from fastapi import FastAPI
 
-#async def notify_message(dp):
+
+
+app = FastAPI() 
+  
+  
+ @app.get("/") 
+ async def root(): 
+     return {"message": "Hello World"}#a
+# def notify_message(dp):
 #    print('Bot is starting')
 #    await dp.bot.send_message(CHAT_ID, text="Bot is starting")
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
     # executor.start_polling(dp, skip_updates=True, on_startup=notify_message)
-    app = FastAPI()
+    #app = FastAPI()

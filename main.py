ba252787fb9a98d37974129bc59c770754e3308f
marsvo1ta <euromobilco@gmail.com>
2023-09-08 11:23:44 +0300

    webhook

--- main.py
+++ main.py
@@ -1,40 +1,39 @@
 import os
-import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.utils import executor
 
+TOKEN = os.environ.get("TELEGRAM_TOKEN")
+WEBHOOK_URL = 'telebot-aio.vercel.app'  # URL вашого Vercel застосунку
+WEBHOOK_PATH = "/webhook"
 
-logging.basicConfig(level=logging.INFO)
-
-# Ініціалізуємо бота та диспетчера
-bot = Bot(token=os.environ.get('TELEGRAM_TOKEN'))
+bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
-dp.middleware.setup(LoggingMiddleware())
+logging_middleware = LoggingMiddleware()
+dp.middleware.setup(logging_middleware)
+
+# Налаштування вебхука
+async def on_start_webhook(dp):
+    await bot.set_webhook(WEBHOOK_URL)
 
 # Обробник команди /start
 @dp.message_handler(commands=['start'])
 async def on_start(message: types.Message):
-    await message.answer("Привіт! Я ваш простий бот на веб-хуках.")
+    await message.answer("Привіт! Я бот, який відповідає на ваші повідомлення.")
 
-# Обробник текстових повідомлень
+# Обробник всіх інших повідомлень
 @dp.message_handler()
-async def echo(message: types.Message):
+async def on_message(message: types.Message):
     await message.answer(f"Ви сказали: {message.text}")
 
 if __name__ == '__main__':
     from aiogram import executor
     executor.start_webhook(
-        dispatcher=dp,
-        webhook_path='/',
-        on_startup=None,
-        on_shutdown=None,
-        host='0.0.0.0',  # Адреса сервера
-        port=3000,  # Порт сервера
+        dp,
+        on_start=on_start_webhook,
+        skip_updates=True,
+        host="0.0.0.0",
+        port=3000,
+        webhook_path=WEBHOOK_PATH
     )
-
-
-
-
-
 

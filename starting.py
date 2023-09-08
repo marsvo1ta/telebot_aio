4ec2923a9057023012eef9569530598a9ec5ee1a
marsvo1ta <84044344+marsvo1ta@users.noreply.github.com>
2023-09-09 00:09:17 +0300

    Update starting.py

--- starting.py
+++ starting.py
@@ -1,23 +1,8 @@
-from aiogram.utils import executor
-#from executor.app import CHAT_ID
-#from executor.handlers import *
-from executor.app import *
-from fastapi import FastAPI
 
+import asyncio
 
+async def main():
+    return "Hello, World!"
 
-#app = FastAPI() 
-  
-  
-#@app.get("/") 
-#async def root():
-app = executor.start_polling(dp, skip_updates=True) 
-    #return {"message": "Hello World"}
-#a
-# def notify_message(dp):
-#    print('Bot is starting')
-#    await dp.bot.send_message(CHAT_ID, text="Bot is starting")
-
-#if __name__ == "__main__":
-    # executor.start_polling(dp, skip_updates=True, on_startup=notify_message)
-    #app = FastAPI()
+if __name__ == "__main__":
+    asyncio.run(main())
